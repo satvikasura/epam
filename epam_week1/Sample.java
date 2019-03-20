a4ae5726df7c82701e6f69c2f7da91ed016f0e04
satvikasura <satvikasura2@gmail.com>
2019-03-21 00:38:30 +0530

    First commit

---
+++ epam_week1/Sample.java
@@ -0,0 +1,15 @@
+package epam_week1;
+/**
+ * 
+ * @author satvika
+ *
+ */
+public class Sample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.print("Welcome to Epam Session");
+	}
+
+}
